3efa2f2e6407271de1af2ea60977526f887f24ed
igorrize <igor.lobazoff@gmail.com>
2026-09-05 01:20:00 +0300

    fish: guard mac-specific GOROOT / omf / uv env for container portability

--- config/fish/conf.d/omf.fish
+++ config/fish/conf.d/omf.fish
@@ -3,5 +3,5 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# Load Oh My Fish configuration (only if installed).
+test -f $OMF_PATH/init.fish; and source $OMF_PATH/init.fish
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,9 @@ end
 # Use Fish completion system instead
 
 set -gx ENV DEVELOPMENT
-set -gx GOROOT $HOME/sdk/go1.26.0
-fish_add_path --prepend $HOME/sdk/go1.26.0/bin
+# Local Go SDK (macOS/dev machine); skip in containers where go lives elsewhere (e.g. /usr/local/go)
+if test -d $HOME/sdk/go1.26.0
+    set -gx GOROOT $HOME/sdk/go1.26.0
+    fish_add_path --prepend $HOME/sdk/go1.26.0/bin
+end
 fish_add_path $HOME/.local/bin  # zj-radar CLI (and other user-local binaries)
